04f2eea28fb86a8afc39fa3b8768aa07585eaff8
SuryaSRMVEC <suryasatz30@gmail.com>
2026-08-25 16:09:30 +0530

    change password component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -37,6 +37,8 @@ const Login = () => {
     }
 
   }   
+
+  const [showPassword, setShowPassword] = useState(false);
   return (
     <div className="m-0 p-0 bg-rose-600 min-h-screen flex justify-center items-center">
      <div className="flex flex-col justify-center items-center py-6 px-10 border m-10 w-[450px] max-w-[90%] bg-pink-600 rounded-xl shadow-xl shadow-pink-300">
@@ -45,10 +47,24 @@ const Login = () => {
           <label className="block m-2 font-bold">Email</label>
           <input type="mail" required="true" name="mail" value={userdata.mail} onChange={handleChange} className="border rounded-md px-3 py-2 w-full bg-white" placeholder="Enter your e-mail" />
         </div>
-        <div className="w-full">
-          <label className="m-2 block font-bold">Password</label>
-          <input type="text" required="true" name="password" value={userdata.password} onChange={handleChange} className="border rounded-md px-3 py-2 bg-white w-full " placeholder="Enter your password"/>
-        </div>
+       <div className="relative">
+            <input
+              type={showPassword ? "text" : "password"}
+              name="password"
+              value={userdata.password}
+              onChange={handleChange}
+              placeholder="Enter your password"
+              className="border rounded-md px-3 py-2 bg-white w-full pr-12"
+            />
+
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute right-3 top-2 text-xl"
+            >
+              {showPassword ? "🙈" : "👁️"}
+            </button>
+          </div>
         <div>
           <p className="p-2 text-xs indent-35 font-bold"> If you dont have Account? <Link to="/signup" className="underline active:text-white hover:text-sky-400 ">Sign Up</Link></p>
         </div>
